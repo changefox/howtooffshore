66e69593a583ffa18fbc99b55185d524bce86b91
Chris WB <chris.wb@changefox.com>
2018-11-27 18:35:59 +0800

    Forms Play

--- public/assets/js/howtooffshore-forms.js
+++ public/assets/js/howtooffshore-forms.js
@@ -1,48 +1,6 @@
 
 // TRELLO SUBMISSION
-$(function() {
-
-    $("#trello input,#trello textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitSuccess: function ($form, event) {
-
-
-
-            var data = {
-                source: 'HOW TO OFFSHORE',
-                type: 'TRELLO REQUEST',
-                name: $('#name').val(),
-                company: $('#company').val(),
-                email: $('#email').val()
-            }
-
-            var $form = $(this);
-            $.post($form.attr("action"), $form.serialize()).then(function() {
-
-                // Hide our form
-                $( "#trello-form" ).hide();
-                
-                // Show our success message!
-                $( "#trello-success" ).show();
-
-            });
-
-            // will not trigger the default submission in favor of the ajax function
-            event.preventDefault();
-        },
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
-        
-    
-});
-
-// NEWSLETTER SUBMISSION
-$("#newsletter").submit(function(e) {
+$("#trello").submit(function(e) {
     e.preventDefault();
 
     var $form = $(this);
@@ -55,57 +13,33 @@ $("#newsletter").submit(function(e) {
     });
 });
 
-// DIRECT REQUEST SUBMISSION
-$(function() {
+// NEWSLETTER SUBMISSION
+$("#newsletter").submit(function(e) {
+    e.preventDefault();
 
-    $("#directrequest input,#directrequest textarea").jqBootstrapValidation({
-        preventSubmit: true,
-        submitSuccess: function ($form, event) {
-
-
-            var URL = 'https://jmkt94us5c.execute-api.us-east-1.amazonaws.com/Production/emailLeadsToChangeFox'
-            var data = {
-            source: 'HOW TO OFFSHORE',
-            type: 'DIRECT REQUEST',
-            name: $('#name').val(),
-            company: $('#company').val(),
-            notes: $('#notes').val(),
-            email: $('#email').val()
-            }
-            $.ajax({
-            type: 'POST',
-            url: URL,
-            dataType: 'json',
-            contentType: 'application/json',
-            data: JSON.stringify(data),
-            success: function () {
-                
-                // Hide our form
-                $( "#directrequest-form" ).hide();
-                
-                // Show our success message!
-                $( "#directrequest-success" ).show();
-            },
-            error: function () {
-                // show an error message
-                alert("Oh my something has gone wrong. You can email us directly on social@howtooffshore.com instead! Sorry for the inconvenience!")
-            }
-            })
-
-            // will not trigger the default submission in favor of the ajax function
-            event.preventDefault();
-        },
-        submitError: function($form, event, errors) {
-            // additional error messages or events
-        },
-        filter: function() {
-            return $(this).is(":visible");
-        },
-    });
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+        // Hide our form
+        $( "#newsletter-form" ).hide();
         
-    
+        // Show our success message!
+        $( "#newsletter-success" ).show();
+    });
 });
 
+// DIRECT REQUEST SUBMISSION
+$("#directrequest").submit(function(e) {
+    e.preventDefault();
+
+    var $form = $(this);
+    $.post($form.attr("action"), $form.serialize()).then(function() {
+        // Hide our form
+        $( "#directrequest-form" ).hide();
+        
+        // Show our success message!
+        $( "#directrequest-success" ).show();
+    });
+});
 
 
 // FIELD UPDATES 
